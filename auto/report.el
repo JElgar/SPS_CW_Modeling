fafda88711de4bf65b6e9591ef7751d78753b719
Jelgar <jamesnelgar@gmail.com>
2020-05-19 16:18:17 +0100

    work on report

---
+++ auto/report.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "report"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "amsmath"
+    "graphicx")
+   (LaTeX-add-labels
+    "fig:noise1"
+    "fig:noise2"
+    "fig:adv3"))
+ :latex)
+
